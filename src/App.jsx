4c4f9fae250b7fe03d54e5d5171d4620cfc628b0
Coro <valeriucolt@gmail.com>
2025-11-26 15:45:26 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "#components/navbar"
+
+const App = () => {
+  return (
+    // <div>
+    //   <h1 className="text-3xl font-bold underline">Bine ai venit</h1>
+    // </div>
+    <main>
+      <Navbar />
+          </main>
+  )
+}
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import dayjs from "dayjs";
+
+import { navIcons, navLinks } from "#constants";
+// import dayjs from "dayjs";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="">
+        <img src="/images/logo.svg" alt="" />
+        <p className="font-bold">portofoliul</p>
+        <ul>
+          {navLinks.map(({ id, name }) => (
+            <li key={id}>{name}</li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <ul>
+            {navIcons.map(({id, img})=>(
+                <li key={id}>
+                    <img src={img} className="icon-hover" alt={`icon-${id}`} />
+                </li>
+            ))}
+        </ul>
+        <time>{dayjs().format("ddd MMM D h:mm A")}</time>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
